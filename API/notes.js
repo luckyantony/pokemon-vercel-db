cce3f40338b63eb627eb83ca75b42f1eba75f27f
luckyantony <luckyantonyofficial@gmail.com>
2025-03-28 23:08:51 +0300

    Changes

---
+++ API/notes.js
@@ -0,0 +1,10 @@
+// Simpler version without process.env
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+export default server;
